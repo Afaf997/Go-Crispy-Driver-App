0d816b2a70e3bc8919790740900aa31282e156f3
Afaf997 <afizaya076@gmail.com>
2024-09-26 07:02:00 +0530

    git

--- lib/utill/images.dart
+++ lib/utill/images.dart
@@ -21,6 +21,7 @@ class Images {
   static const String email = 'assets/image/email.png';
   static const String send = 'assets/image/send.png';
   static const String image = 'assets/image/image.png';
+  static const String appicon = 'assets/image/App.png';
   static const String audios = 'assets/image/audio.wav';
 
 
